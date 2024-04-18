b9904c042d5c0796b40753648539dd8b7a162ad7
ahmadqayyum7 <166563161+ahmadqayyum7@users.noreply.github.com>
2024-04-18 12:11:53 +0500

    Updated before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd /home/ec2-user/server
+# navigate to app folder
+cd /app
 
 # install node and npm
 apt-get install curl
-curl -sL https://rpm.nodesource.com/setup_20.x | sudo -E bash -
-yum -y install nodejs npm
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+apt-get install nodejs -y
+apt-get install npm -y
+apt install nginx -y
+ufw allow 'Nginx HTTP'
