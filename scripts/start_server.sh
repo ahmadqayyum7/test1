48cf216e4f020abc23342a284374c62dd004796a
ahmadqayyum7 <166563161+ahmadqayyum7@users.noreply.github.com>
2024-04-18 18:44:39 +0500

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,9 +6,9 @@
 # # initial startup by running react-script "start", name process "marketing"
 # # --watch watches and restarts if files change
 # npm start
-# pm2 start
-# pm2 startup
-# pm2 save
-# pm2 restart all
+pm2 start
+pm2 startup
+pm2 save
+pm2 restart all
 systemctl -l enable nginx
 systemctl -l start nginx
