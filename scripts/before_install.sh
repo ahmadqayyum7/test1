82dcff64985391c36c88123c71cbd96ff9822e54
ahmadqayyum7 <166563161+ahmadqayyum7@users.noreply.github.com>
2024-04-18 18:51:00 +0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
+ 
+    # navigate to app folder
+    cd /app
 
-# navigate to app folder
-cd /app
-
-# install node and npm
-apt-get install curl
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-apt-get install nodejs -y
-apt-get install npm -y
-apt install nginx -y
-
-# Enable Nginx to start automatically on system boot
-systemctl enable nginx
-
-# Start Nginx
-systemctl start nginx
-
-# Allow HTTP traffic through the firewall
-ufw allow 'Nginx HTTP'
+    # install node and npm
+    apt-get install curl
+    curl -fsSL https://deb.nodesource.com/setup_20.x | sudo - 
+    E bash -
+    apt-get install nodejs -y
+    apt-get install npm -y
+    apt install nginx -y
+    ufw allow 'Nginx HTTP'
