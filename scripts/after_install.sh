2f1614f0e81459d2dbd62645d3dc9a0af828541a
ahmadqayyum7 <166563161+ahmadqayyum7@users.noreply.github.com>
2024-04-18 18:50:37 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# navigate to app folder
-cd /app
+    # navigate to app folder
+    cd /app
 
-# install dependencies
-npm install
-npm run build
-cp -r build/* /var/www/html
-npm install pm2 -g
+    # install dependencies
+    npm install
+    npm run build
+    cp -r build/* /var/www/html
+    npm install pm2 -g
