420a18f912383fbacb513075bb615a8e778e2652
Ubuntu <ubuntu@ip-172-31-82-151.ec2.internal>
2024-08-23 14:04:24 +0000

    Changed the output

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello.
+          Deploying through Jenkins.
         </p>
         <a
           className="App-link"
