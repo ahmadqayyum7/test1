e40c869d3cada87c96be33ad552965291f58c25e
ahmadqayyum7 <ahmadqayyum19@gmil.com>
2025-04-26 18:40:58 +0500

    Changed output

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deploying through Jenkins.
+          Deploying.
         </p>
         <a
           className="App-link"
